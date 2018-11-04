6ec4269217a9e2aed7c21aa31eb5056629fcd0f8
gyoungis <gyoungis@iastate.edu>
2018-11-04 10:57:41 -0600

    Added files

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+    CharacterController controller;
+
+    public Camera cam;                                                                                        
+
+    private float jumpSpeed = 20.0F;
+    private float gravity = 5.0F;
+    private float speed = 7.0f;
+    private float jumpMove;
+    private Vector3 moveDirection = Vector3.zero;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        jumpMove = 0.3f * speed;
+        // Hides mouse cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // Determines movement of player
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+            {
+                gameObject.transform.forward = new Vector3(cam.transform.forward.x, gameObject.transform.forward.y, cam.transform.forward.z);
+            }
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        //else
+        //{
+        //    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        //    {
+        //        gameObject.transform.forward = new Vector3(cam.transform.forward.x, gameObject.transform.forward.y, cam.transform.forward.z);
+        //    }
+        //    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //    moveDirection = transform.TransformDirection(moveDirection);
+
+        //    moveDirection.x *= jumpMove;
+        //    moveDirection.z *= jumpMove;
+        //}
+
+        // Final player Movement
+        moveDirection.y -= gravity*gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);        
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Transform camTransform;
+
+    // Movement Variables
+    private float moveSpeed = 5f;
+    private float rotationSpeed = 1f;
+    private Rigidbody rb;
+
+    // Jump Variables
+    private float jumpSpeed = 8.0f;
+    private float gravity = 20.0f;
+    private bool singleJump = false;
+    private bool doubleJump = false;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Move Forward
+            moveDir += camTransform.forward;
+            gameObject.transform.forward = camTransform.forward;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Move Backward
+            moveDir += -camTransform.forward;
+            gameObject.transform.forward = camTransform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Move to the Left
+            moveDir += -camTransform.right;
+            gameObject.transform.forward = -camTransform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Move to the Right
+            moveDir += camTransform.right;
+            gameObject.transform.forward = camTransform.right;
+        }
+        //moveDir.y = moveDir.y -= gravity * Time.deltaTime; ;
+        moveDir.y = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Jump
+            if (!doubleJump)
+            {
+                if (singleJump)
+                {
+                    moveDir.y = jumpSpeed;
+                    singleJump = false;
+                    doubleJump = true;
+                }
+                else
+                {
+                    moveDir.y = jumpSpeed;
+                    singleJump = true;
+                }
+            }
+        }
+        transform.position += moveDir.normalized * moveSpeed * Time.deltaTime;
+
+        if (moveDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(moveDir), rotationSpeed * Time.deltaTime);
+        }
+    }
+}
